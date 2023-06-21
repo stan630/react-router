527c72792eb054ff4882600b6cb7f2fe5fa8c4bf
Stanley <cslafond@gmail.com>
2023-06-21 11:54:29 -0400

    update api.js

--- src/api.js
+++ src/api.js
@@ -1,16 +1,15 @@
 export async function getVans(id) {
-    const url = id ? `/api/vans/${id}`: "/api/vans"
+    const url = id ? `/api/vans/${id}` : "/api/vans"
     const res = await fetch(url)
     if (!res.ok) {
         throw {
             message: "Failed to fetch vans",
-            statusText: resStatusText,
+            statusText: res.statusText,
             status: res.status
         }
     }
     const data = await res.json()
     return data.vans
-      
 }
 
 export async function getHostVans(id) {
@@ -19,11 +18,10 @@ export async function getHostVans(id) {
     if (!res.ok) {
         throw {
             message: "Failed to fetch vans",
-            statusText: resStatusText,
+            statusText: res.statusText,
             status: res.status
         }
     }
     const data = await res.json()
     return data.vans
-      
 }
